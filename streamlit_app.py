805b36ee3985e154a7439aa7b283486210d9b49b
Roxana Bostan <119664982+RoxBostan@users.noreply.github.com>
2022-12-02 12:36:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom'''''s New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.title('Breakfast Favorites')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard Boiled Free-Range Egg')
+streamlit.text(':avocado:Avocado Toast')
